09d4b0fce25bc8329ddff44b0270ba32b0abb68d
hjo <hahn.jo12@gmail.com>
2019-03-01 19:27:22 -0800

    complete

--- 0_Nemean_Lion/build_and_run.sh
+++ 0_Nemean_Lion/build_and_run.sh
@@ -1,2 +1,5 @@
+#!/bin/zsh
+docker-machine restart hercules
+eval $(docker-machine env hercules)
 docker build -t nemean_lion .
 docker run --interactive --tty nemean_lion
--- 0_Nemean_Lion/script.sh
+++ 0_Nemean_Lion/script.sh
@@ -1,4 +1,3 @@
-cat /etc/ssh/ssh_config | sed 's/Port 22/Port 4242' > .my_sshd_config
-mv .my_sshd_config /etc/ssh/sshd_config
+sed -i -e 's/#Port 22/Port 4242/' /etc/ssh/sshd_config
 service ssh restart
 # ss -l to check if port number has changed

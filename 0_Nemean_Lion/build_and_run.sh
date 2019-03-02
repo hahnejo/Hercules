57068419961a77a2d78dcdc24017c7983f455a62
hjo <hahn.jo12@gmail.com>
2019-03-01 19:39:14 -0800

    complete

--- 0_Nemean_Lion/build_and_run.sh
+++ 0_Nemean_Lion/build_and_run.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-docker-machine restart hercules
+docker-machine create -d virtualbox hercules
+docker-machine start hercules
 eval $(docker-machine env hercules)
 docker build -t nemean_lion .
 docker run --interactive --tty nemean_lion

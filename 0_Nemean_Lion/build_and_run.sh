b6b8f3a59f8840fc75df537f19836b2ad65458e5
hjo <hahn.jo12@gmail.com>
2019-03-01 19:49:10 -0800

    introduction

--- 0_Nemean_Lion/build_and_run.sh
+++ 0_Nemean_Lion/build_and_run.sh
@@ -1,4 +1,9 @@
+# Before running this script, be ware!
+# Make sure to install docker-machine and docker!
+# Also make sure to have virtualbox installed.
+
 #!/bin/zsh
+
 docker-machine create -d virtualbox hercules
 docker-machine start hercules
 eval $(docker-machine env hercules)
